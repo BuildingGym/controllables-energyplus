31aaa97827e0f7d0308a23070a7383e973aab099
sysadmin <sysadmin@homelab-hvac>
2023-12-26 22:56:24 -0800

    sync

---
+++ packages/energyplus/ooep/addons/state/__init__.py
@@ -0,0 +1,63 @@
+import contextlib
+import typing
+
+import threading
+
+from ... import ems
+from . import callback_queue
+
+
+class BaseStateMachine:
+    class StepFunction:
+        def __init__(
+            self, 
+            state_machine: 'BaseStateMachine',
+            event_specs: ems.Environment.Event.Specs
+        ):
+            self._callback = callback_queue.CloseableCallbackQueue()
+            self._state_machine = state_machine
+            self._event_specs = event_specs
+        
+        @contextlib.contextmanager
+        def __state_context__(self):
+            self._state_machine._env.event_listener.subscribe(
+                self._event_specs,
+                self._callback,
+            )
+            yield
+            self._callback.close()
+
+        def __call__(self, func: typing.Callable | None = None):
+            return self._callback.call(func)
+
+    def __init__(
+        self, 
+        env: ems.Environment
+    ):
+        self._env = env
+        self._step_funcs: typing.List[self.StepFunction] = []
+
+    def step_function(self, event_specs):
+        f = self.StepFunction(self, event_specs)
+        self._step_funcs.append(f)
+        return f
+
+    def run_blocking(self, *args, **kwargs):
+        with contextlib.ExitStack() as stack:
+            for f in self._step_funcs:
+                stack.enter_context(f.__state_context__())
+            return self._env(*args, **kwargs)
+
+class StateMachine(BaseStateMachine):
+    def run(self, *args, **kwargs):
+        thr = threading.Thread(
+            target=self.run_blocking, 
+            args=args, kwargs=kwargs,
+        )
+        thr.start()
+
+
+__all__ = [
+    BaseStateMachine,
+    StateMachine,
+]
---
+++ packages/energyplus/ooep/addons/state/callback_queue.py
@@ -0,0 +1,58 @@
+import queue
+
+import dataclasses
+import typing
+
+
+class CallbackQueue:
+    @dataclasses.dataclass
+    class Task:
+        func: typing.Callable | None = None
+        result: typing.Any = None
+
+    def __init__(self, maxsize=1):
+        self.base = queue.Queue(maxsize=maxsize)
+
+    def __call__(self, *args, **kwargs):
+        task: self.Task = self.base.get()
+        if task.func is not None:
+            task.result = task.func(*args, **kwargs)
+        self.base.task_done()
+
+    def call(self, func: typing.Callable | None = None):
+        task = self.Task(func=func)
+        self.base.put(task)
+        self.base.join()
+        return task.result
+
+class CloseableCallbackQueue(CallbackQueue):
+    class Closed(Exception):
+        pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.closed = False
+
+    def __call__(self, *args, **kwargs):
+        if self.closed:
+            raise self.Closed()
+        return super().__call__(*args, **kwargs)
+    
+    def call(self, func: typing.Callable):
+        if self.closed:
+            raise self.Closed()
+        return super().call(func)
+    
+    def close(self):
+        self.closed = True
+        # NOTE ref https://stackoverflow.com/a/18873213
+        while not self.base.empty():
+            try: self.base.get(block=False)
+            except queue.Empty: continue
+            self.base.task_done()
+
+
+__all__ = [
+    CallbackQueue,
+    CloseableCallbackQueue,
+]
